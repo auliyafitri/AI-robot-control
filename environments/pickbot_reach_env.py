2183ca1310691b2d309813d9aa32dbb7878886b0
YangLiu14 <lylander14@gmail.com>
2019-05-16 15:25:49 +0200

    ad busy waiting

--- environments/pickbot_reach_env.py
+++ environments/pickbot_reach_env.py
@@ -804,6 +804,8 @@ class PickbotEnv(gym.Env):
         if joint_exceeds_limits:
             print("is_done: Joints: {}".format(np.round(self.joints_state.position, decimals=3)))
             self.publisher_to_moveit_object.pub_joints_to_moveit([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+            while not self.movement_complete.data:
+                pass
             self.publisher_to_moveit_object.pub_relative_joints_to_moveit(joint_correction)
             while not self.movement_complete.data:
                 pass
