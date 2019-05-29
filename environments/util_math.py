85894bd8cb467de9579b605955b0a9b2361d8679
YangLiu14 <lylander14@gmail.com>
2019-05-29 12:52:23 +0200

    add different plans to compute reward

--- environments/util_math.py
+++ environments/util_math.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 
 def compute_reward(observation, done_reward, invalid_contact):
@@ -14,27 +15,46 @@ def compute_reward(observation, done_reward, invalid_contact):
     Done Reward:    Reward when episode is Done. Negative Reward for Crashing and going into set Joint Limits.
                     High Positiv Reward for having contact with both contact sensors and not having an invalid collision
     """
-    reward_contact = 0
 
-    # Reward for Distance to encourage approaching the box
+    ####################################################################
+    #                        Plan0: init                               #
+    ####################################################################
+
+    # reward_contact = 0
+    #
+    # # Reward for Distance to encourage approaching the target object
+    # distance = observation[0]
+    # # reward_distance = 1 - math.pow(distance / max_distance, 0.4)
+    # relative_distance = observation[-1] - distance
+    # reward_distance = relative_distance * 20 if relative_distance < 0 else relative_distance * 10
+    #
+    # # Reward for Contact
+    # contact_1 = observation[7]
+    # contact_2 = observation[8]
+    #
+    # if contact_1 == 0 and contact_2 == 0:
+    #     reward_contact = 0
+    # elif contact_1 != 0 and contact_2 == 0 and not invalid_contact or contact_1 == 0 and contact_2 != 0 and \
+    #         not invalid_contact:
+    #     reward_contact = 2000
+    #     reward_distance = 0
+    #
+    # total_reward = reward_distance + reward_contact + done_reward
+    #
+    # print("reward_distance: {}".format(reward_distance))
+    #
+    # return total_reward
+
+    ####################################################################################
+    # Plan1: Reach a point in 3D space (usually right above the target object)         #
+    # Reward only dependent on distance. Nu punishment for crashing or joint_limits    #
+    ####################################################################################
     distance = observation[0]
-    # reward_distance = 1 - math.pow(distance / max_distance, 0.4)
-    relative_distance = observation[-1] - distance
-    reward_distance = relative_distance * 20 if relative_distance < 0 else relative_distance * 10
-
-    # Reward for Contact
-    contact_1 = observation[7]
-    contact_2 = observation[8]
-
-    if contact_1 == 0 and contact_2 == 0:
-        reward_contact = 0
-    elif contact_1 != 0 and contact_2 == 0 and not invalid_contact or contact_1 == 0 and contact_2 != 0 and \
-            not invalid_contact:
-        reward_contact = 2000
-        reward_distance = 0
-
-    total_reward = reward_distance + reward_contact + done_reward
-
-    print("reward_distance: {}".format(reward_distance))
+    x = np.sqrt((1/3)) * distance
+    alpha = 5
+    done = 0.02
+    a = np.exp(-alpha*x) - np.exp(-alpha) + 10 * (np.exp(-alpha*x / done) - np.exp(-alpha))
+    b = 1 - np.exp(-alpha)
+    total_reward = a/b - 1
 
     return total_reward
