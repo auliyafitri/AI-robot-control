57f515de4e14b3709bd77413ac5450e9ef2828e5
YangLiu14 <lylander14@gmail.com>
2019-07-04 16:12:18 +0200

    add training for cnn_training

--- experiments/reach_cnn/DeepQ/train_DeepQ_discrete_cnn.py
+++ experiments/reach_cnn/DeepQ/train_DeepQ_discrete_cnn.py
@@ -3,10 +3,11 @@ import rospy
 import datetime
 
 
-from environments import pickbot_reach_env, gazebo_connection
+from environments import gazebo_connection
+from environments.pickbot_reach_cam_env import PickbotReachCamEnv
 
 from baselines import deepq
-timestamp=datetime.datetime.now()
+timestamp = datetime.datetime.now()
 
 
 def callback(lcl, _glb):
@@ -18,13 +19,26 @@ def callback(lcl, _glb):
 def main():
     # unpause Simulation so that robot receives data on all topics
     gazebo_connection.GazeboConnection().unpauseSim()
-    # create node
     rospy.init_node('pickbot_gym', anonymous=True, log_level=rospy.FATAL)
-    env = gym.make("Pickbot-v2")
 
+    env = PickbotReachCamEnv(is_discrete=True)
+    model = deepq.models.cnn_to_mlp(convs=[(32, 8, 4), (64, 4, 2), (64, 3, 1)],
+                                    hiddens=[256],
+                                    dueling=False)
+    act = deepq.learn(
+        env,
+        network=model,
+        lr=1e-3,
+        total_timesteps=1000000000,
+        buffer_size=50000,
+        exploration_fraction=0.1,
+        exploration_final_eps=0.02,
+        print_freq=10,
+        callback=callback,
+    )
+    print("Saving model to kuka_cam_model.pkl")
+    act.save("kuka_cam_model.pkl")
 
-    print("Saving model to pickbot_model_"+str(timestamp)+".pkl")
-    # act.save("pickbot_model_"+str(timestamp)+".pkl")
 
 if __name__ == '__main__':
     main()
